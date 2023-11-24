2234020f30be76abb0265e6a7eccd22b1c824372
Muhammad Kashif <mkashif.aslam36@gmail.com>
2023-11-24 20:41:58 +0500

    barcode scanning init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Html5QrcodeScanner} from "html5-qrcode";
+import {Html5QrcodeResult} from "html5-qrcode/src/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'barcode-scanner';
+  scanResult: Html5QrcodeResult = {decodedText: '', result: {text: ''}};
+  decodedText: string = '';
+
+  ngOnInit() {
+    let html5QrcodeScanner = new Html5QrcodeScanner(
+      "reader",
+      {fps: 10, qrbox: {width: 250, height: 250}},
+      false);
+    html5QrcodeScanner.render((decodedText: string, decodedResult: Html5QrcodeResult) => {
+      console.log(`Code matched = ${decodedText}`, decodedResult);
+      this.decodedText = decodedText;
+      this.scanResult = decodedResult;
+    }, (error: unknown) => {
+      // handle scan failure, usually better to ignore and keep scanning.
+      // console.warn(`Code scan error = ${error}`);
+    });
+  }
 }
